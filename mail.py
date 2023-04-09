1eb0fee827c2116bceb53ab5997f27ed26686880
Tiansheng Huang <71095600+Harold-Tiansheng@users.noreply.github.com>
2023-04-09 01:10:30 -0400

    Update mail.py
    now you can customize reply content and forward address

--- mail.py
+++ mail.py
@@ -61,24 +61,53 @@ def list_inbox(current_id):
             documents = [message['body']['content']]
             response = client.extract_key_phrases(documents = documents)[0]
             #print(response.key_phrases)
+
             if not response.is_error:
                 has_send = False
                 for phrase in response.key_phrases:
-                    if 'Accommodation' in phrase or 'accommodation' in phrase or 'Accomodation' in phrase or 'accomodation' in phrase:
-                        graph.do_forward(message['id'], 'h1255740727@gmail.com', sent_message)
-                        content = '''
+
+                    def ruleSetting(topic, isForward = False, content = "this is default content", forwardEmail = None):
+                        if topic.lower() in phrase.lower():
+                            global has_send
+                            if isForward == True:
+                                graph.do_forward(message['id'], forwardEmail, sent_message)
+                            graph.send_mail('Reply:' + message['subject'], content,reply_address)
+                            print("forwarding and auto reply process completed!")
+                            global has_send
+                            has_send = True
+                    #usage: ruleSetting(topic, isForward, content, forwardEmail)
+                    #topic: keyword or topic you want to search
+                    #isForward: if you want this email to be forwarded to somebody, put this boolean value to true, otherwise false
+                    #content: content of auto reply
+                    #forwardEmail: forward email address, if you put false in second argument, leave it blank
+
+                    #example 1
+                    ruleSetting("accommodation", True, '''
                                 <head>Note: this is an auto reply according to email content you provide, hope these information will help you. </head><br>
                             -	<a href="https://www.uwo.ca/univsec/pdf/academic_policies/appeals/Academic%20Accommodation_disabilities.pdf" target="_blank">Academic Accommodation for Students with Disabilities</a><br>
                             -	<a href="https://accessibility.uwo.ca/faculty_staff/policies_programs.html" target="_blank">Accessibility at Western </a><br>
                             -	<a href="https://www.uwo.ca/hr/diversity/accommodate.html" target="_blank">Duty to Accommodate Guidelines </a><br>
                             -	<a href="https://www.uwo.ca/health/" target="_blank">Health and Wellness</a><br>
                             -	<a href="https://www.uwo.ca/coronavirus/" target="_blank">COVID </a><br>
-                                <head> If you want help from staff, we will contact with you soon </head><br>
-                        '''
-                        graph.send_mail('Reply:' + message['subject'], content,reply_address)
-                        print("forwarding and auto reply process completed!")
-                        has_send = True
-                        break
+                                <head> if you need further help, we will contact with you soon </head><br>
+                        ''', 'someemail@gmail.com')
+                    
+                    #example 2
+                    ruleSetting('Course',True, '''
+                                <head>Note: this is an auto reply according to email content you provide, hope these information will help you. </head><br>
+                            -	<a href="https://www.edu.uwo.ca/csw/bed/index.html" target="_blank">Course Outlines</a><br>
+                            -	<a href="https://theteachercandidate.com/schedules/" target="_blank">Course Schedules</a><br>
+                                <head> if you need further help, we will contact with you soon </head><br>
+                        ''', 'someemail@gmail.com')
+                    
+                    #example 3
+                    ruleSetting('Scholarships',False, '''
+                                <head>Note: this is an auto reply according to email content you provide, hope these information will help you. </head><br>
+                            -	<a href="https://www.edu.uwo.ca/csw/bed/index.html" target="_blank">Awards & Scholarships Website</a><br>
+                            -	<a href="https://registrar.uwo.ca/student_finances/index.html" target="_blank">Financial Aid Website</a><br>
+                                <head> if you need further help, we will contact with you soon </head><br>
+                        ''')
+
                 if not has_send:
                     print("no keyword contain! leave in the mail box")
                     #do nothing
